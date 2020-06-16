aa3553a86d60ea74ac0dd43845ae1caa4ddbe9eb
tuily <brizolla.tuily@gmail.com>
2020-06-16 20:19:45 -0300

    Refactor RepoList

--- client/src/components/RepoList/index.js
+++ client/src/components/RepoList/index.js
@@ -3,25 +3,24 @@ import React from 'react';
 import Repo from '../Repo';
 
 const RepoList = ({ repositories = [], loading = true }) => {
-    console.log('repolist: ', repositories);
     return (
         <div id='repo-list' className='container-sm'>
             {loading && (
                 <div className='text-center anim-fade-in'>
                     <h2 className='mt-4'>
                         <span>Loading</span>
-                        <span class='AnimatedEllipsis'></span>
+                        <span className='AnimatedEllipsis'></span>
                     </h2>
                 </div>
             )}
 
             {repositories.map((repo) => (
-                <Repo id={repo.id} name={repo.name} description={repo.description} url={repo.html_url} />
+                <Repo key={repo.id} id={repo.id} name={repo.name} description={repo.description} url={repo.html_url} />
             ))}
 
             {!loading && repositories.length === 0 && (
-                <div class='blankslate text-center anim-fade-in'>
-                    <h3 class='mb-1'>Hmmm, parece que voce não adicionou nenhuma estrela!</h3>
+                <div className='blankslate text-center anim-fade-in'>
+                    <h3 className='mb-1'>Hmmm, parece que voce não adicionou nenhuma estrela!</h3>
                     <p>Adicione estrelas em alguns repositórios e depois volte aqui.</p>
                     <a href='https://github.com/trending'>Ver repositórios</a>
                 </div>
