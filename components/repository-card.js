353c60ca7960095df585c076aad9f5df0ec5fb97
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-06-27 15:16:35 -0300

    repos: add list to starred repositories

---
+++ components/repository-card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Text, Stack, Badge, Button } from '@chakra-ui/core'
+
+export function RepositoryCard({ name, description, url, tags }) {
+  return (
+    <Stack bg="white" borderWidth={1} borderRadius="4px" p={4} boxShadow="sm">
+      <Text fontSize="2xl" fontWeight="bold" color="pink.400">
+        {name}
+      </Text>
+
+      <Text fontSize="lg">{description}</Text>
+
+      <Button as="a" href={url} color="twitter.400" target="_blank" mt="auto">
+        Ver repo
+      </Button>
+
+      <Stack isInline spacing={2}>
+        {tags?.map(tag => (
+          <Badge key={tag}>{tag}</Badge>
+        ))}
+      </Stack>
+    </Stack>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
-import { Flex } from '@chakra-ui/core'
+import { Flex, Grid } from '@chakra-ui/core'
+import { useQuery } from 'react-query'
 import { User } from '../components/user'
+import { RepositoryCard } from '../components/repository-card'
 import { useUser } from '../context/user'
+import { FullpageSpinner } from '../components/spinner'
+
+const getStarredRepositoriesUrl = data => {
+  const {
+    items: [user],
+  } = data
+  return `https://api.github.com/users/${user.login}/starred`
+}
 
 function IndexPage() {
   const { user } = useUser()
+
+  const { data: starred } = useQuery('starred', async () => {
+    const data = await fetch(
+      `https://api.github.com/search/users?q=${user.email}`
+    )
+
+    return data.json()
+  })
+
+  const { data: repositories, status, isFetching } = useQuery(
+    ['repositories', starred],
+    async (__, starred) => {
+      const url = getStarredRepositoriesUrl(starred)
+      const data = await fetch(url)
+
+      return await data.json()
+    },
+    {
+      enabled: starred,
+    }
+  )
 
   return (
     <Flex direction="column">
@@ -14,6 +45,21 @@ function IndexPage() {
         width="100%"
       >
         <User name={user?.name} image={user?.image} />
+      </Flex>
+
+      <Flex flexWrap="wrap">
+        {(status === 'loading' || isFetching) && <FullpageSpinner />}
+
+        <Grid
+          width="100%"
+          gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))"
+          gridGap={4}
+          p={4}
+        >
+          {repositories?.map(repo => (
+            <RepositoryCard key={repo.id} {...repo} />
+          ))}
+        </Grid>
       </Flex>
     </Flex>
   )
