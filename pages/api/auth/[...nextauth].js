0727d291eeb1520dec299664cadc21874042aad4
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2020-06-27 15:15:45 -0300

    auth: add secret to json

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import Providers from 'next-auth/providers'
 
 const options = {
   site: process.env.DOMAIN || 'http://localhost:3000',
+  secret: `iamapotato`,
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
