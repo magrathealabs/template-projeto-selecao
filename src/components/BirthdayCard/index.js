5995eca8e324d6f769ca8bcffa0b6e046acf4be5
Leonardo Kalyn <leonardo.kalyn@gmail.com>
2020-07-11 22:32:06 -0300

    Add error message to Home

--- src/components/BirthdayCard/index.js
+++ src/components/BirthdayCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ClassNames from 'classnames'
+import PropTypes from 'prop-types';
 
 import './style.scss';
 import '../../styles/flip.scss';
@@ -9,31 +10,58 @@ const BirthdayCard = ({
   cardSelected,
   date,
   birthdays,
+  error,
 }) => (
   <div
     onClick={handleClick}
     className={ClassNames(
       'birthday-card',
-      {'birthday-card--selected': cardSelected},
+      {
+        'birthday-card--selected': cardSelected,
+        'birthday-card--error': !!error,
+      },
     )}
   >
-    <span className="birthday-card__date">
-      {date.format('dddd')}
-    </span>
-    <span className="birthday-card__date">
-      {date.format('DD/MMM')}
-    </span>
-    <ul>
-      {birthdays.length
-        ? birthdays.map((birthday, birthdayIndex) => (
-          <li key={birthdayIndex} className="birthday-card__name">
-            {birthday}
-          </li>
-        ))
-        : <li>No Birthdays today :(</li>
-      }
-    </ul>
+    {!!error
+      ? (
+        <>
+          <p className="birthday-card__error">
+            The following error occurred:
+          </p>
+          <p className="birthday-card__error">
+            {error}
+          </p>
+        </>
+      ) : (
+        <>
+          <span className="birthday-card__date">
+            {date.format('dddd')}
+          </span>
+          <span className="birthday-card__date">
+            {date.format('DD/MMM')}
+          </span>
+          <ul>
+            {birthdays.length
+              ? birthdays.map((birthday, birthdayIndex) => (
+                <li key={birthdayIndex} className="birthday-card__name">
+                  {birthday}
+                </li>
+              ))
+              : <li>No Birthdays today :(</li>
+            }
+          </ul>
+        </>
+      )
+    }
   </div>
 );
+
+BirthdayCard.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  cardSelected: PropTypes.bool.isRequired,
+  date: PropTypes.object,
+  birthdays: PropTypes.array,
+  error: PropTypes.string,
+};
 
 export default BirthdayCard;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
@@ -17,6 +18,7 @@ const Home =  ({
   fetchBirthdays,
   week,
   year,
+  error,
 }) => {
 
   const [showModal, setShowModal] = useState(false);
@@ -87,41 +89,63 @@ const Home =  ({
       </div>
 
       <div className="birthday__card-list">
-        {weekList.map((day, index) => (
-          <div className="birthday__card-container" key={index}>
-            <div className="balloon"/>
-            <CSSTransition
-              in={inProp}
-              timeout={400}
-              classNames="flip"
-            >
-              <BirthdayCard
-                handleClick={() => selectCard(index === selectedCard ? -1 : index)}
-                date={moment(day.date)}
-                birthdays={day.birthdays}
-                cardSelected={selectedCard === index}
-              />
-            </CSSTransition>
-          </div>
-        ))}
+        { !!error
+          ? (
+            <div className="birthday__card-container">
+              <div className="balloon"/>
+              <CSSTransition
+                in={inProp}
+                timeout={400}
+                classNames="flip"
+              >
+                <BirthdayCard
+                  handleClick={() => fetchBirthdays(week, year)}
+                  error={error}
+                  cardSelected
+                />
+              </CSSTransition>
+            </div>
+          ) : weekList.map((day, index) => (
+            <div className="birthday__card-container" key={index}>
+              <div className="balloon"/>
+              <CSSTransition
+                in={inProp}
+                timeout={400}
+                classNames="flip"
+              >
+                <BirthdayCard
+                  handleClick={() => selectCard(index === selectedCard ? -1 : index)}
+                  date={moment(day.date)}
+                  birthdays={day.birthdays}
+                  cardSelected={selectedCard === index}
+                />
+              </CSSTransition>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
 }
 
-const mapStateToProps = ({birthdays : {
-  results,
-  isFetching,
-  isLoaded,
-  week,
-  year,
-}}) => ({
-  birthdays: results,
-  isLoaded,
-  isFetching,
-  week,
-  year,
+const mapStateToProps = (state) => ({
+  birthdays: state.birthdays.results,
+  isLoaded: state.birthdays.isLoaded,
+  isFetching: state.birthdays.isFetching,
+  week: state.birthdays.week,
+  year: state.birthdays.year,
+  error: state.birthdays.error,
 });
+
+Home.propTypes = {
+  birthdays: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  isLoaded: PropTypes.bool.isRequired,
+  fetchBirthdays: PropTypes.func.isRequired,
+  week: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  error: PropTypes.string,
+}
 
 export default connect(
   mapStateToProps,
