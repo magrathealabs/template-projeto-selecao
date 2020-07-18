1d24c4c08c204aa4636164f385a88a1b24349656
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-07-17 22:36:33 -0300

    feat: add function to parse string a array

---
+++ backend/src/utils/parseStringAsArray.ts
@@ -0,0 +1,9 @@
+export default function parseStringAsArray(arrayAsString: string) {
+  if (arrayAsString.includes(',')) {
+    const tagsArray = arrayAsString.split(',').map(tag => tag.trim());
+
+    return [...new Set(tagsArray)];
+  } else {
+    return arrayAsString.split(' ');
+  }
+}
