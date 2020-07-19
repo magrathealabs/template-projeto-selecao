7038db67ca0ed908609af3eba188da8e76c44912
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-07-19 15:24:22 -0300

    feat: update fetch hook

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -12,11 +12,15 @@ export default function useFetch<Data = any, Error = any>(fetchArguments: string
   const { data, error, mutate } = useSWR<Data, Error>([url, filter], async url => {
     const data = JSON.parse(localStorage.getItem('@Magrathea:repositories') || '[]');
 
-    if (!data || filter !== '') {
+    if (data.length === 0 || filter !== '') {
       const response = await api.get(url, {
         headers: { authorization: token, id: user.id, login: user.login },
         params: { filter },
       });
+
+      if (filter === '') {
+        localStorage.setItem('@Magrathea:repositories', JSON.stringify(response.data));
+      }
 
       return response.data;
     }
