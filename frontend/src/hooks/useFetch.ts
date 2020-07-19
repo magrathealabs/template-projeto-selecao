5fdec94c90282231bb25436e8f01677b5d6b3bbb
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-07-19 14:45:54 -0300

    feat: update name at hooks

---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import useSWR from 'swr';
+
+import api from '../services/api';
+
+import { useAuth } from './useAuth';
+
+export default function useFetch<Data = any, Error = any>(fetchArguments: string[]) {
+  const { token, user } = useAuth();
+
+  const [url, filter] = fetchArguments;
+
+  const { data, error, mutate } = useSWR<Data, Error>([url, filter], async url => {
+    const data = JSON.parse(localStorage.getItem('@Magrathea:repositories') || '[]');
+
+    if (!data || filter !== '') {
+      const response = await api.get(url, {
+        headers: { authorization: token, id: user.id, login: user.login },
+        params: { filter },
+      });
+
+      return response.data;
+    }
+
+    return data;
+  });
+
+  return { data, error, mutate };
+}
