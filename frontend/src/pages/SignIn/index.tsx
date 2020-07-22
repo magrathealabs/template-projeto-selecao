f5ecb16ffeee3bb503f85e254b9f56bec3125df5
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-07-22 14:59:17 -0300

    feat: add load in sign in

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 
+import Load from '../../components/Load';
 import Template from '../../components/Template';
 
 import './styles.scss';
@@ -9,14 +10,18 @@ import './styles.scss';
 import { useAuth } from '../../hooks/useAuth';
 
 const SignIn: React.FC = () => {
+  const [load, setLoad] = useState(false);
+
   const { signIn } = useAuth();
 
   const history = useHistory();
 
   const handleSignInWithGithub = useCallback(
     async code => {
+      setLoad(true);
       await signIn(code);
 
+      setLoad(false);
       history.push('/repositories');
     },
     [signIn, history],
@@ -33,25 +38,29 @@ const SignIn: React.FC = () => {
 
   return (
     <Template>
-      <main>
-        <section>
-          <h2>login</h2>
-          <div>
-            <div className="login__card">
-              <h3>Github</h3>
-              <a href="http://github.com/login/oauth/authorize?client_id=0c8bf460469b9af72df7">
-                login with github <FiChevronRight />
-              </a>
+      {load ? (
+        <Load />
+      ) : (
+        <main>
+          <section>
+            <h2>login</h2>
+            <div>
+              <div className="login__card">
+                <h3>Github</h3>
+                <a href="http://github.com/login/oauth/authorize?client_id=0c8bf460469b9af72df7">
+                  login with github <FiChevronRight />
+                </a>
+              </div>
+              <div className="login__card --facebook">
+                <h3>Facebook</h3>
+                <a href="/">
+                  login with facebook <FiChevronRight />
+                </a>
+              </div>
             </div>
-            <div className="login__card --facebook">
-              <h3>Facebook</h3>
-              <a href="/">
-                login with facebook <FiChevronRight />
-              </a>
-            </div>
-          </div>
-        </section>
-      </main>
+          </section>
+        </main>
+      )}
     </Template>
   );
 };
