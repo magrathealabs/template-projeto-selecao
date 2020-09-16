9c372e4c69f0b2a70ccfff867e8c09cdfb9789c9
Peter Brendel <peter_brendel@hotmail.com>
2020-09-16 00:20:52 -0300

    Changes to main file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/Navbar';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from './components/card/Card';
+
 
 function App() {
+
+    const [tags, setTags] = useState(
+        [
+            {
+                variant:'secondary', 
+                text: 'Javascript'
+            },
+            {
+                variant:'success',
+                text: 'C++'
+            },
+            {
+                variant:'secondary',
+                text: "Ana"
+            },
+            {
+                variant:'light',
+                text: "ovo"
+            },
+            {
+                variant:'light',
+                text: "ovo"
+            }
+        ]
+    );
 
     return (
         <div className="App">
             <Navbar/>
+            <CardDeck className="Card-folder">
+                <Card
+                repository="Test Repo"
+                author="unkown"
+                readme="Little experimental text to check if card is doing ok"
+                tags={tags}/>
+
+                <Card
+                repository="Test Repo"
+                author="unkown"
+                readme="Little experimental text to check if card is doing ok"
+                tags={tags}/>
+
+                <Card
+                repository="Test Repo"
+                author="unkown"
+                readme="Little experimental text to check if card is doing ok"
+                tags={tags}/>
+
+                <Card
+                repository="Test Repo"
+                author="unkown"
+                readme="Little experimental text to check if card is doing ok"
+                tags={tags}/>
+
+                <Card
+                repository="Test Repo"
+                author="unkown"
+                readme="Little experimental text to check if card is doing ok"
+                tags={tags}/>
+            </CardDeck>
         </div>
     );
 }
