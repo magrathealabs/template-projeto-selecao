8fe0fd4c2f83976e382ca6f2172f8cdba702bd0d
Peter Brendel <peter_brendel@hotmail.com>
2021-02-18 17:54:41 -0300

    Fixing bug injected on app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 
 let configService = new ConfigService();
 
 @Module({
   imports: [UsersModule, ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(configService.get('DB_URL'))],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
