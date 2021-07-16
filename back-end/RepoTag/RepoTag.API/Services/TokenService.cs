8141837de0053499cc222af0db512937874ffb53
eusougz <guilhermegms209@gmail.com>
2021-07-16 11:34:05 -0300

    Add API w/ temporary Auth config

---
+++ back-end/RepoTag/RepoTag.API/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RepoTag.Domain;
+using RepoTag.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoTag.API.Services
+{
+    public class TokenService
+    {
+        private readonly Settings _settings;
+
+        public TokenService(Settings settings)
+        {
+            _settings = settings;
+        }
+        
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_settings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
