ea6c0b5dd4502b1169f7f11b780589fada8b4dcf
eusougz <guilhermegms209@gmail.com>
2021-07-18 21:32:48 -0300

    [fix] change Angular version storybook is having problems w/ Angular 12

--- front-end/repo-tag/src/app/app.module.ts
+++ front-end/repo-tag/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
